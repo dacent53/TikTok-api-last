d15ff13efd805f02074696ef538e5d6845ee12b8
dacent53 <gneo0194@gmail.com>
2026-01-12 19:53:06 +0300

    Update api.py

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 # testing api
-**Contact for api :** [https://t.me/alethino](https://t.me/alethino)
-## telegram: @alethino
+**Contact for api :** [https://t.me/dacent53](https://t.me/dacent53)
+## telegram: @dacent53
 
 import requests
 import time
